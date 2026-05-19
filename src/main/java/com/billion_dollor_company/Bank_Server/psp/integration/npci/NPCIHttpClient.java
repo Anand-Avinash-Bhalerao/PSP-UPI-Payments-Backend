6566d419b00a0a88a908b3aa3ad642cdb9d83173
anandbhalerao <anandbhalerao.l.a3b@gmail.com>
2026-05-19 17:30:04 +0530

    Refactor NPCIHttpClient to use RestTemplate.exchange for check balance requests and handle errors with detailed response parsing

--- src/main/java/com/billion_dollor_company/Bank_Server/psp/integration/npci/NPCIHttpClient.java
+++ src/main/java/com/billion_dollor_company/Bank_Server/psp/integration/npci/NPCIHttpClient.java
@@ -7,9 +7,17 @@ import com.billion_dollor_company.Bank_Server.psp.domain.CheckBalanceResult;
 import com.billion_dollor_company.Bank_Server.psp.integration.npci.dto.checkBalance.NPCICheckBalanceReqBodyDTO;
 import com.billion_dollor_company.Bank_Server.psp.integration.npci.dto.checkBalance.NPCICheckBalanceResBodyDTO;
 import com.billion_dollor_company.Bank_Server.psp.integration.npci.mapper.NPCIMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
@@ -20,6 +28,9 @@ public class NPCIHttpClient implements NPCIClient {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Value("${npci.base-url}")
     private String baseUrl;
@@ -33,12 +44,37 @@ public class NPCIHttpClient implements NPCIClient {
 
         final String url = baseUrl + checkBalanceEndpoint;
 
-        // Map CheckBalanceCommand to NPCI request format
         BaseRequestDTO<NPCICheckBalanceReqBodyDTO> request = npciMapper.toNPCICheckBalanceReq(command);
 
-        // make HTTP call to NPCI endpoint.
-        BaseResponseDTO<NPCICheckBalanceResBodyDTO> response = restTemplate.postForObject("", request, BaseResponseDTO.class);
+        try {
 
-        return npciMapper.toCheckBalanceResult(response);
+            ResponseEntity<BaseResponseDTO<NPCICheckBalanceResBodyDTO>> response =
+                    restTemplate.exchange(
+                            url,
+                            HttpMethod.POST,
+                            new HttpEntity<>(request),
+                            new ParameterizedTypeReference<>() {
+                            }
+                    );
+
+            return npciMapper.toCheckBalanceResult(response.getBody());
+
+        } catch (HttpStatusCodeException ex) {
+
+            String responseBody = ex.getResponseBodyAsString();
+
+            BaseResponseDTO<NPCICheckBalanceResBodyDTO> errorResponse = null;
+            try {
+                errorResponse = objectMapper.readValue(responseBody, new TypeReference<>() {});
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+
+            return npciMapper.toCheckBalanceResult(errorResponse);
+
+        } catch (Exception ex) {
+            throw ex;
+        }
     }
+
 }
